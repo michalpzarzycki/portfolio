1a0ec814d3f5b60fbd9e07db4f64fc9561ea242f
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-02-12 23:28:36 +0100

    Set the react-router-dom and creted navbar and home components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowseRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowseRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Home}/>
+        </Switch>
+        </BrowseRouter>
     </div>
   );
 }

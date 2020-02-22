27fcd919b5dc1515a48e8bf39158e8478ad26944
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-02-22 22:24:54 +0100

    Changed the navbar styles and main App(added content class)

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <BrowserRouter>
         <FirebaseContext.Provider value={{ user, firebase }}>
         <Navbar />
-        <div style={{marginTop:"30px"}}>
+        <div className="content">
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/login" component={Login}/>

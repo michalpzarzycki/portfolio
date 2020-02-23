d42cb8066804323b80309b4d51d6f8128b69d818
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-02-23 08:02:07 +0100

    Added fadein animation on components load

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Home.module.css'
 
@@ -6,9 +6,11 @@ function Home() {
 
     return(
         <div className={styles.mainDiv}>
-            <div className={styles.welcomeSection}>
+            <div  className={styles.welcomeSection}>
+                <div className={styles.content}>
                 <Link to="/">WELCOME!</Link>
                 <p>A SHORT DESCRIPTION</p>
+                </div>
             </div>
         </div>
     )
